96cb4c0080fa3d7d02a51070b440412ba2f90825
garotoprograma <canaltitan778@gmail.com>
2025-09-02 05:02:51 -0300

    organização

--- todolist/src/main/java/br/com/curso/todolistapi/tarefa/TarefaRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.curso.todolistapi.tarefa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
